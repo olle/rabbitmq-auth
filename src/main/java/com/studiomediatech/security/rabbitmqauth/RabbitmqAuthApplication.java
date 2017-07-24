5673ad3e02f31f127985cedd204262fed04d7299
Olle Törnström <olle@studiomediatech.com>
2017-07-24 18:22:32 +0200

    Initial starter-skeleton

---
+++ src/main/java/com/studiomediatech/security/rabbitmqauth/RabbitmqAuthApplication.java
@@ -0,0 +1,12 @@
+package com.studiomediatech.security.rabbitmqauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqAuthApplication.class, args);
+	}
+}
---
+++ src/test/java/com/studiomediatech/security/rabbitmqauth/RabbitmqAuthApplicationTests.java
@@ -0,0 +1,16 @@
+package com.studiomediatech.security.rabbitmqauth;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RabbitmqAuthApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
