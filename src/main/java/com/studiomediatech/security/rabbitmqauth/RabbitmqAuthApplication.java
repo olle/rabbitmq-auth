d61d20115dedeb2fd4cf98b90d8e1142588c36b4
Olle Törnström <olle@studiomediatech.com>
2017-09-13 22:25:49 +0200

    Update to latest spring boot

--- src/main/java/com/studiomediatech/security/rabbitmqauth/RabbitmqAuthApplication.java
+++ src/main/java/com/studiomediatech/security/rabbitmqauth/RabbitmqAuthApplication.java
@@ -1,5 +1,8 @@
 package com.studiomediatech.security.rabbitmqauth;
 
+import java.util.Objects;
+import java.util.Optional;
+
 import org.springframework.amqp.core.AbstractDeclarable;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
@@ -9,12 +12,20 @@ import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.RememberMeAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 @SpringBootApplication
@@ -22,6 +33,39 @@ public class RabbitmqAuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqAuthApplication.class, args);
+	}
+
+	@Bean
+	public AuthenticationProvider our() {
+
+		return new AuthenticationProvider() {
+
+			@Override
+			public boolean supports(Class<?> authentication) {
+				return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+			}
+
+			@Override
+			public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+				if (Objects.nonNull(authentication)) {
+					String credentials = (String) authentication.getCredentials();
+					authentication.setAuthenticated(credentials.equalsIgnoreCase("foobar"));
+				}
+
+				return authentication;
+			}
+		};
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	public static class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().antMatchers("/**").fullyAuthenticated().and().formLogin().permitAll();
+		}
 	}
 
 	@Configuration
